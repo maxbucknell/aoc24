93ea0ae2eb9ba7dcc065340d5a12f77a6ccb872b
Max Bucknell <me@maxbucknell.com>
2024-12-01 17:45:09 +1100

    Add test harness and basic file structure

---
+++ lib/aoc/day_01.ex
@@ -0,0 +1,15 @@
+defmodule AOC.Day01 do
+  @doc ~S"""
+  Solution to Day 1, Part 1
+
+  See [question here](https://adventofcode.com/2024/day/1)
+
+  ## Examples
+
+    iex> AOC.Day01.a()
+    {:ok, 1}
+  """
+  def a do
+    {:ok, 1}
+  end
+end
---
+++ test/aoc/day_01_test.exs
@@ -0,0 +1,5 @@
+defmodule AOC.Day01Test do
+  use ExUnit.Case, async: true
+
+  doctest AOC.Day01
+end
