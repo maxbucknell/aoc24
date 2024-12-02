e67536fa3cfa250077ed8581e9556d82b016b1fd
Max Bucknell <me@maxbucknell.com>
2024-12-02 21:33:45 +1100

    Day 2, Part A

---
+++ test/aoc/day_02/parser_test.exs
@@ -0,0 +1,5 @@
+defmodule AOC.Day02.ParserTest do
+  use ExUnit.Case, async: true
+
+  doctest AOC.Day02.Parser
+end
---
+++ test/aoc/day_02/validator_test.exs
@@ -0,0 +1,5 @@
+defmodule AOC.Day02.ValidatorTest do
+  use ExUnit.Case
+
+  doctest AOC.Day02.Validator
+end
