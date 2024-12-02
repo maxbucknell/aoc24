10eaaffa475e2e3bccc64f83718eadb9ac2da0cc
Max Bucknell <me@maxbucknell.com>
2024-12-02 17:17:10 +1100

    Day 1, part A

--- lib/aoc/day_01.ex
+++ lib/aoc/day_01.ex
@@ -6,10 +6,35 @@ defmodule AOC.Day01 do
 
   ## Examples
 
-    iex> AOC.Day01.a()
-    {:ok, 1}
+    iex> input = [
+    ...>   "3   4",
+    ...>   "4   3",
+    ...>   "2   5",
+    ...>   "1   3",
+    ...>   "3   9",
+    ...>   "3   3",
+    ...> ]
+    iex> AOC.Day01.a(input)
+    {:ok, 11}
   """
-  def a(_) do
-    {:ok, 1}
+  def a(input) do
+    {left, right} =
+      input
+      |> Stream.map(fn line ->
+        {:ok, output} = AOC.Day01.Parser.parse_line(line)
+
+        output
+      end)
+      |> Enum.unzip()
+
+    left = Enum.sort(left)
+    right = Enum.sort(right)
+
+    result =
+      Stream.zip([left, right])
+      |> Stream.map(fn {a, b} -> abs(a - b) end)
+      |> Enum.sum()
+
+    {:ok, result}
   end
 end
