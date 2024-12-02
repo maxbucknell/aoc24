a10ad7fbfbe74cd9679efff0d31c850e079f2b1b
Max Bucknell <me@maxbucknell.com>
2024-12-02 17:38:31 +1100

    Day 1, part B

--- lib/aoc/day_01.ex
+++ lib/aoc/day_01.ex
@@ -18,14 +18,7 @@ defmodule AOC.Day01 do
     {:ok, 11}
   """
   def a(input) do
-    {left, right} =
-      input
-      |> Stream.map(fn line ->
-        {:ok, output} = AOC.Day01.Parser.parse_line(line)
-
-        output
-      end)
-      |> Enum.unzip()
+    {left, right} = AOC.Day01.Parser.parse_input(input)
 
     left = Enum.sort(left)
     right = Enum.sort(right)
@@ -33,6 +26,39 @@ defmodule AOC.Day01 do
     result =
       Stream.zip([left, right])
       |> Stream.map(fn {a, b} -> abs(a - b) end)
+      |> Enum.sum()
+
+    {:ok, result}
+  end
+
+  @doc ~S"""
+  Solution to Day 1, Part 2
+
+  See [question here](https://adventofcode.com/2024/day/1)
+
+  ## Examples
+
+    iex> input = [
+    ...>   "3   4",
+    ...>   "4   3",
+    ...>   "2   5",
+    ...>   "1   3",
+    ...>   "3   9",
+    ...>   "3   3",
+    ...> ]
+    iex> AOC.Day01.b(input)
+    {:ok, 31}
+  """
+  def b(input) do
+    {left, right} = AOC.Day01.Parser.parse_input(input)
+
+    occurrences =
+      right
+      |> Enum.reduce(%{}, fn n, acc -> Map.update(acc, n, 1, &(&1 + 1)) end)
+
+    result =
+      left
+      |> Enum.map(fn n -> n * Map.get(occurrences, n, 0) end)
       |> Enum.sum()
 
     {:ok, result}
