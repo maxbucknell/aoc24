f8db45794561e26412e7455eb840f2c189295463
Max Bucknell <max@baseup.com.au>
2024-12-05 19:28:12 +1100

    Add reader module for part B

---
+++ lib/aoc/day_04/reader/b.ex
@@ -0,0 +1,5 @@
+defmodule AOC.Day04.Reader.B do
+  def read_line(line, state \\ %{}) do
+
+  end
+end
---
+++ test/aoc/day_04/reader/b_test.exs
@@ -0,0 +1,5 @@
+defmodule AOC.Day04.Reader.BTest do
+  use ExUnit.Case, async: true
+
+  doctest AOC.Day04.Reader.B
+end
