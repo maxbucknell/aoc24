5027771a327825f978989d72797991817639757c
Max Bucknell <me@maxbucknell.com>
2024-12-09 16:09:57 +1100

    Fix day 3

--- lib/aoc/day_03.ex
+++ lib/aoc/day_03.ex
@@ -2,13 +2,16 @@ defmodule AOC.Day03 do
   alias AOC.Day03.Parser
 
   @doc ~S"""
-    iex> input = String.codepoints("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+    iex> input = [
+    ...>   "xmul(2,4)%&mul[3,\n",
+    ...>   "7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))\n"
+    ...> ]
     iex> AOC.Day03.a(input)
     {:ok, 161}
   """
   def a(input) do
     result =
-      input
+      Stream.concat(Stream.map(input, &String.graphemes/1))
       |> Enum.reduce({[], nil}, fn x, {vals, state} ->
         case Parser.A.parse(state, x) do
           {:val, n} -> {vals ++ [n], nil}
@@ -22,13 +25,16 @@ defmodule AOC.Day03 do
   end
 
   @doc ~S"""
-    iex> input = String.codepoints("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+    iex> input = [
+    ...>   "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)\n",
+    ...>   "+mul(32,64](mul(11,8)undo()?mul(8,5))\n"
+    ...> ]
     iex> AOC.Day03.b(input)
     {:ok, 48}
   """
   def b(input) do
     result =
-      input
+      Stream.concat(Stream.map(input, &String.graphemes/1))
       |> Enum.reduce({[], nil}, fn x, {vals, state} ->
         case Parser.B.parse(state, x) do
           {:val, n} -> {vals ++ [n], nil}
